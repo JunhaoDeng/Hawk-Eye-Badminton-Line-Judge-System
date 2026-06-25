6d11e27f9754a0b3d62e42489bca777916083e61
JunhaoDeng <48834693+JunhaoDeng@users.noreply.github.com>
2026-06-25 14:41:36 +0800

    增加pm2启动方式

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  apps: [
+    {
+      name: 'badminton-server',
+      script: 'index.js',
+      cwd: path.join(__dirname, 'server'),
+      max_memory_restart: '2G',
+      env: {
+        NODE_ENV: 'dev',
+        JWT_SECRET: 'your_jwt_secret_key_here',
+      },
+    },
+    {
+      name: 'badminton-frontend',
+      script: path.join(__dirname, 'frontend/node_modules/.bin/vite'),
+      args: '--host 0.0.0.0 --port 3000',
+      cwd: path.join(__dirname, 'frontend'),
+      max_memory_restart: '1G',
+    },
+  ],
+};
